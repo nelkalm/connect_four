190d4e5b03c7486fa569949161f8da6364deeafe
Nelson Lu <nelkalmlu@gmail.com>
2022-04-27 06:37:30 -0700

    Built the board, with refactored code.

--- connect_four.py
+++ connect_four.py
@@ -1,61 +1,47 @@
 # Initialize Player1 and Player2 symbols
 player_symbol = 'X'
 player_symbol_real = [' ', 'X', '0']
+num_columns = 7
+
 
 # Create column labels
-numbers = [i for i in range(1, 8)]
-column_label = list()
-for number in numbers:
-    column_label.append([' ' * 2, str(number), ' '])
-column_output = ''
-for item in column_label:
-    column_output += ''.join(item)
-print(column_output)
+def create_column_labels(num_columns):
+    numbers = [i for i in range(1, num_columns + 1)]
+    column_label = list()
+    for number in numbers:
+        column_label.append([' ' * 2, str(number), ' '])
+    column_output = ''
+    for item in column_label:
+        column_output += ''.join(item)
+    print(column_output)
+
+
+# Create container structures
+def create_container_structure(character_structure_list, num_columns):
+    created_structure = list()
+    for _ in range(1, num_columns + 1):
+        created_structure.append(character_structure_list)
+    created_structure_output = ''
+    for item in created_structure:
+        created_structure_output += ''.join(item)
+    created_structure_output += character_structure_list[0]
+    print(created_structure_output)
+
 
 # Initialize the character structure of the container
 top_edge = ['+', '-' * 3]
 non_interactable_row = ['|', ' ' * 3]
 interactable_row = ['|', ' ', player_symbol, ' ']
 
-# Create the lid of the container
-lid_structure = list()
-for _ in range(1, 8):
-    lid_structure.append(top_edge)
-lid_structure_output = ''
-for item in lid_structure:
-    lid_structure_output += ''.join(item)
-# Add the last element to the lid structure
-lid_structure_output += '+'
-
-# print(lid_structure_output)
-
-# Create the side of the container
-side_structure = list()
-for _ in range(1, 8):
-    side_structure.append(non_interactable_row)
-side_structure_output = ''
-for item in side_structure:
-    side_structure_output += ''.join(item)
-# Add the last element to the side structure
-side_structure_output += '|'
-# print(side_structure_output)
-
-# Create the row containing the location of X and O
-side_structure_interact = list()
-for _ in range(1, 8):
-    side_structure_interact.append(interactable_row)
-side_structure_interact_output = ''
-for item in side_structure_interact:
-    side_structure_interact_output += ''.join(item)
-# Add the last element to the side structure
-side_structure_interact_output += '|'
+# Create column labels
+create_column_labels(num_columns)
 
 # Iterate 6x for lids to create the structure of the board
-for _ in range(1, 7):
-    print(lid_structure_output)
-    print(side_structure_output)
-    print(side_structure_interact_output)
-    print(side_structure_output)
-print(lid_structure_output)
+for _ in range(1, num_columns):
+    create_container_structure(top_edge, num_columns)
+    create_container_structure(non_interactable_row, num_columns)
+    create_container_structure(interactable_row, num_columns)
+    create_container_structure(non_interactable_row, num_columns)
+create_container_structure(top_edge, num_columns)
 
 
